e7df3a24cf1ce6e06264e31ffce5c1fac65b2e67
Mago Minimalista <contato@magominimalista.com.br>
2023-05-20 09:38:06 -0300

    Detalhe header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Header } from './components/Header/Header'
 import { Hero } from './components/Hero/Hero'
+import './App.css'
 
 function App() {
     return (
-        <div className="app">
-            <Header />
-            <Hero />
+        <div className="App">
+            <div>
+                <div className="white-gradient" />
+                <Header />
+                <Hero />
+            </div>
         </div>
     )
 }
