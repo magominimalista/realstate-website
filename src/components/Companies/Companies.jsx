c4b2ffcc0c98d1ce7cfdeec59ad8b62485ba197c
Mago Minimalista <contato@magominimalista.com.br>
2023-05-20 09:48:00 -0300

    sessão companies

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Companies.css'
+
+export const Companies = () => {
+    return (
+        <div>
+            <section className="c-wrapper">
+                <div className="paddings innerWidth flexCenter c-container">
+                    <img src="./prologis.png" alt="place" />
+                    <img src="./tower.png" alt="place" />
+                    <img src="./equinix.png" alt="place" />
+                    <img src="./realty.png" alt="place" />
+                </div>
+            </section>
+        </div>
+    )
+}
